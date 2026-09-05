3cfced27c4d42169f0314591accd4881bcc65027
Joao francisco Azevedo Zupeli <zupelidev@gmail.com>
2026-09-05 16:00:51 -0700

    feat: Anne Andreatta Advocacia demo from advocproto (batch 4)

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { refreshOnAssetsLoad } from './lib/gsap'
+import './assets/styles/main.css'
+
+const app = createApp(App).use(router)
+
+router.isReady().then(() => {
+  app.mount('#app')
+  refreshOnAssetsLoad()
+})
